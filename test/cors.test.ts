94c232f4329ee97d3a92fd540e4110cf1a7d4fd2
antony1060 <antoniostignjedec@gmail.com>
2021-07-26 16:15:09 +0200

    Added cors check to tests

---
+++ test/cors.test.ts
@@ -0,0 +1,11 @@
+import supertest from "supertest"
+import app from "../src/app"
+
+describe("CORS", () => {
+    it("allows every origin", (done) => {
+        supertest(app)
+            .get("/")
+            .expect("Access-Control-Allow-Origin", "*")
+            .expect(200, done)
+    })
+})
--- test/util/common.ts
+++ test/util/common.ts
@@ -25,8 +25,11 @@ export async function buildConentRequests(content: string) {
 }
 
 export function modifyFields(json: { [key: string]: any }) {
+    // delete the `requestIp` because some ISPs don't give a static ip and tests might fail because of that
+    delete json["requestIp"];
+
     // remove unnecessary headers
-    for(const f of ["accept", "cf-ray", "user-agent"])
+    for(const f of ["accept", "cf-ray", "user-agent", "x-forwarded-for", "cf-connecting-ip"])
         if(f in json["headers"])
             delete json["headers"][f]
 

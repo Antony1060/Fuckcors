8a72eb926acd7d1059e407412a82a9569a92bec6
Antony1060 <antoniostignjedec@gmail.com>
2021-08-15 19:25:15 +0200

    fixed test

--- test/general/frontend.test.ts
+++ test/general/frontend.test.ts
@@ -4,16 +4,6 @@ import supertest from "supertest";
 import app from "../../src/app";
 
 describe("FRONTEND", () => {
-    it("should return a static html file", (done) => {
-        supertest(app)
-            .get("/")
-            .expect(200, async (err, res) => {
-                if(err) return done(err);
-                expect(res.text)
-                    .toEqual(await fs.readFile("public/index.html", { encoding: "utf-8" }));
-                done();
-            });
-    });
 
     it("should grab assets", (done) => {
         supertest(app)
